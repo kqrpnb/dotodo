2867148403cf4e84f0b51e5a04bee74c2a089f7f
drvicki <vicki@digitalcrafts.com>
2022-07-08 10:04:56 -0400

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dotodo';
+
+  filter: 'all' | 'active' | 'done'='all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  private newMethod_1() {
+    return this;
+  }
+
+  private newMethod(): string | undefined {
+    return '/item.component.html';
+  }
+
+  get items() {
+    if (this.filter === 'all')
+{
+  return this.allItems;
+}
+return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+}
+
+addItem(description: string) {
+  this.allItems.unshift({
+    description,
+    done: false
+  });
+}
+
+remove(item: { description: string; done: boolean; }) {
+  this.allItems.splice(this.allItems.indexOf(item), 1);
+}
+}
